55140b626fd36a866a8bcbc8b47a3cc16d68e2f8
zakhodyaeva <margarita.zakhodyaeva@devexpress.com>
2021-08-03 16:58:50 +0300

    update solutions

--- CS/Form1.cs
+++ CS/Form1.cs
@@ -1,48 +1,92 @@
 ﻿using DevExpress.XtraCharts;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Forms;
 
 namespace WindowsFormsApplication1 {
     public partial class Form1 : Form {
-        public Form1() {
-            InitializeComponent();
+        const int HistogramPointsCount = 1000;
+        const int DistributionPointsCount = 100;
+        const double Mean = 0.5;
+        const double StdDev = 0.15;
+        const double Max = 10;
+        const double MinValue = 0;
+        const double MaxValue = Max;
+        const int BinCount = 20;
+        Random random = new Random();
 
-            List<double> histogramData = new List<double>();
-            List<TestPoint> lineData = new List<TestPoint>();
-            Random random = new Random();
-            for(int i = 0; i < 100; i++) {
-                int value = random.Next(1, 20);
-                histogramData.Add(value);
-                lineData.Add(new TestPoint(value, random.Next(1, 10)));
-            }
+        public List<DataPoint> NormalDistribution { get; private set; }
+        public List<DataPoint> Histogram { get; private set; }
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            CreateDataSource();
 
             Series histogram = new Series("Histogram", ViewType.Bar);
-            histogram.DataSource = histogramData;
+            histogram.ArgumentDataMember = "XValue";
             chartControl1.Series.Add(histogram);
             ((BarSeriesView)histogram.View).AggregateFunction = SeriesAggregateFunction.Histogram;
 
-            Series line = new Series("Line", ViewType.Line);
-            line.DataSource = lineData;
-            line.ArgumentDataMember = "Argument";
-            line.ValueDataMembers[0] = "Value";
+            Series line = new Series("Line", ViewType.Spline);
+            line.ArgumentDataMember = "XValue";
+            line.ValueDataMembers[0] = "YValue";
             chartControl1.Series.Add(line);
             LineSeriesView lineView = (LineSeriesView)line.View;
-            lineView.AggregateFunction = SeriesAggregateFunction.Average;
-
+            lineView.AggregateFunction = SeriesAggregateFunction.None;
             XYDiagram diagram = (XYDiagram)chartControl1.Diagram;
             diagram.AxisX.NumericScaleOptions.ScaleMode = ScaleMode.Interval;
+            diagram.AxisX.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            diagram.AxisX.WholeRange.MinValue = MinValue;
+            diagram.AxisX.WholeRange.MaxValue = MaxValue;
+            diagram.AxisX.WholeRange.SideMarginsValue = 0;
+            diagram.AxisX.NumericScaleOptions.IntervalOptions.GridLayoutMode = GridLayoutMode.GridShiftedLabelCentered;
+            diagram.AxisX.NumericScaleOptions.IntervalOptions.Count = BinCount;
+            diagram.AxisX.NumericScaleOptions.IntervalOptions.DivisionMode = IntervalDivisionMode.Count;
+            diagram.AxisX.Label.TextPattern = "{}{OB}{A1:F1}, {A2:F1}{CB}";
             SecondaryAxisY secondaryAxisY = new SecondaryAxisY();
             diagram.SecondaryAxesY.Add(secondaryAxisY);
             lineView.AxisY = secondaryAxisY;
+            SecondaryAxisX secondaryAxisX = new SecondaryAxisX();
+            diagram.SecondaryAxesX.Add(secondaryAxisX);
+            secondaryAxisX.WholeRange.SideMarginsValue = 0;
+            lineView.AxisX = secondaryAxisX;
+
+            line.DataSource = NormalDistribution;
+            histogram.DataSource = Histogram;
+
+        }
+
+        void CreateDataSource() {
+            NormalDistribution = new List<DataPoint>();
+            for (int i = 0; i < DistributionPointsCount; i++) {
+                double x = i * Max / DistributionPointsCount;
+                NormalDistribution.Add(new DataPoint(x, GetNormalDistribution(x, Mean * Max, StdDev * Max)));
+            }
+            Histogram = new List<DataPoint>();
+            for (int x = 0; x < HistogramPointsCount; x++)
+                Histogram.Add(new DataPoint(GenerateHistogramPoint(Mean * Max, StdDev * Max)));
+        }
+        double GetNormalDistribution(double x, double mean, double stdDev) {
+            double tmp = 1 / (Math.Sqrt(2 * Math.PI) * stdDev);
+            return Math.Exp(-Math.Pow(x - mean, 2) / (2 * Math.Pow(stdDev, 2))) * tmp;
+        }
+        double GenerateHistogramPoint(double mean, double stdDev) {
+            
+            return Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Cos(2 * Math.PI * random.NextDouble()) * stdDev + mean;
         }
     }
-    public class TestPoint {
-        public double Argument { get; }
-        public double Value { get; }
-        public TestPoint(double argument, double value) {
-            Argument = argument;
-            Value = value;
+    public class DataPoint {
+        public double XValue { get; set; }
+        public double YValue { get; set; }
+
+        public DataPoint(double x, double y) {
+            XValue = x;
+            YValue = y;
+        }
+        public DataPoint(double value) : this(value, value) {
         }
     }
 }
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,50 +1,112 @@
 ﻿Imports DevExpress.XtraCharts
 Imports System
 Imports System.Collections.Generic
+Imports System.Collections.ObjectModel
 Imports System.Windows.Forms
 
 Namespace WindowsFormsApplication1
 	Partial Public Class Form1
 		Inherits Form
 
+		Private Const HistogramPointsCount As Integer = 1000
+		Private Const DistributionPointsCount As Integer = 100
+		Private Const Mean As Double = 0.5
+		Private Const StdDev As Double = 0.15
+		Private Const Max As Double = 10
+		Private Const MinValue As Double = 0
+		Private Const MaxValue As Double = Max
+		Private Const BinCount As Integer = 20
+		Private random As New Random()
+
+		Private privateNormalDistribution As List(Of DataPoint)
+		Public Property NormalDistribution() As List(Of DataPoint)
+			Get
+				Return privateNormalDistribution
+			End Get
+			Private Set(ByVal value As List(Of DataPoint))
+				privateNormalDistribution = value
+			End Set
+		End Property
+		Private privateHistogram As List(Of DataPoint)
+		Public Property Histogram() As List(Of DataPoint)
+			Get
+				Return privateHistogram
+			End Get
+			Private Set(ByVal value As List(Of DataPoint))
+				privateHistogram = value
+			End Set
+		End Property
+
+
 		Public Sub New()
 			InitializeComponent()
+			CreateDataSource()
 
-			Dim histogramData As New List(Of Double)()
-			Dim lineData As New List(Of TestPoint)()
-			Dim random As New Random()
-			For i As Integer = 0 To 99
-				Dim value As Integer = random.Next(1, 20)
-				histogramData.Add(value)
-				lineData.Add(New TestPoint(value, random.Next(1, 10)))
-			Next i
+'INSTANT VB NOTE: The variable histogram was renamed since Visual Basic does not handle local variables named the same as class members well:
+			Dim histogram_Renamed As New Series("Histogram", ViewType.Bar)
+			histogram_Renamed.ArgumentDataMember = "XValue"
+			chartControl1.Series.Add(histogram_Renamed)
+			CType(histogram_Renamed.View, BarSeriesView).AggregateFunction = SeriesAggregateFunction.Histogram
 
-			Dim histogram As New Series("Histogram", ViewType.Bar)
-			histogram.DataSource = histogramData
-			chartControl1.Series.Add(histogram)
-			CType(histogram.View, BarSeriesView).AggregateFunction = SeriesAggregateFunction.Histogram
-
-			Dim line As New Series("Line", ViewType.Line)
-			line.DataSource = lineData
-			line.ArgumentDataMember = "Argument"
-			line.ValueDataMembers(0) = "Value"
+			Dim line As New Series("Line", ViewType.Spline)
+			line.ArgumentDataMember = "XValue"
+			line.ValueDataMembers(0) = "YValue"
 			chartControl1.Series.Add(line)
 			Dim lineView As LineSeriesView = CType(line.View, LineSeriesView)
-			lineView.AggregateFunction = SeriesAggregateFunction.Average
-
+			lineView.AggregateFunction = SeriesAggregateFunction.None
 			Dim diagram As XYDiagram = CType(chartControl1.Diagram, XYDiagram)
 			diagram.AxisX.NumericScaleOptions.ScaleMode = ScaleMode.Interval
+			diagram.AxisX.Visibility = DevExpress.Utils.DefaultBoolean.True
+			diagram.AxisX.WholeRange.MinValue = MinValue
+			diagram.AxisX.WholeRange.MaxValue = MaxValue
+			diagram.AxisX.WholeRange.SideMarginsValue = 0
+			diagram.AxisX.NumericScaleOptions.IntervalOptions.GridLayoutMode = GridLayoutMode.GridShiftedLabelCentered
+			diagram.AxisX.NumericScaleOptions.IntervalOptions.Count = BinCount
+			diagram.AxisX.NumericScaleOptions.IntervalOptions.DivisionMode = IntervalDivisionMode.Count
+			diagram.AxisX.Label.TextPattern = "{}{OB}{A1:F1}, {A2:F1}{CB}"
 			Dim secondaryAxisY As New SecondaryAxisY()
 			diagram.SecondaryAxesY.Add(secondaryAxisY)
 			lineView.AxisY = secondaryAxisY
+			Dim secondaryAxisX As New SecondaryAxisX()
+			diagram.SecondaryAxesX.Add(secondaryAxisX)
+			secondaryAxisX.WholeRange.SideMarginsValue = 0
+			lineView.AxisX = secondaryAxisX
+
+			line.DataSource = NormalDistribution
+			histogram_Renamed.DataSource = Histogram
+
 		End Sub
+
+		Private Sub CreateDataSource()
+			NormalDistribution = New List(Of DataPoint)()
+			For i As Integer = 0 To DistributionPointsCount - 1
+				Dim x As Double = i * Max / DistributionPointsCount
+				NormalDistribution.Add(New DataPoint(x, GetNormalDistribution(x, Mean * Max, StdDev * Max)))
+			Next i
+			Histogram = New List(Of DataPoint)()
+			For x As Integer = 0 To HistogramPointsCount - 1
+				Histogram.Add(New DataPoint(GenerateHistogramPoint(Mean * Max, StdDev * Max)))
+			Next x
+		End Sub
+		Private Function GetNormalDistribution(ByVal x As Double, ByVal mean As Double, ByVal stdDev As Double) As Double
+			Dim tmp As Double = 1 / (Math.Sqrt(2 * Math.PI) * stdDev)
+			Return Math.Exp(-Math.Pow(x - mean, 2) / (2 * Math.Pow(stdDev, 2))) * tmp
+		End Function
+		Private Function GenerateHistogramPoint(ByVal mean As Double, ByVal stdDev As Double) As Double
+
+			Return Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Cos(2 * Math.PI * random.NextDouble()) * stdDev + mean
+		End Function
 	End Class
-	Public Class TestPoint
-		Public ReadOnly Property Argument() As Double
-		Public ReadOnly Property Value() As Double
-		Public Sub New(ByVal argument As Double, ByVal value As Double)
-			Me.Argument = argument
-			Me.Value = value
+	Public Class DataPoint
+		Public Property XValue() As Double
+		Public Property YValue() As Double
+
+		Public Sub New(ByVal x As Double, ByVal y As Double)
+			XValue = x
+			YValue = y
+		End Sub
+		Public Sub New(ByVal value As Double)
+			Me.New(value, value)
 		End Sub
 	End Class
 End Namespace
