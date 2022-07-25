c423ad77e01495b34500f651420a0e24cf067733
DevExpressExampleBot <fakeEmail>
2022-07-25 17:46:14 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,50 +1,53 @@
-﻿Imports DevExpress.XtraCharts
+Imports DevExpress.XtraCharts
 Imports System
+
 Namespace WindowsFormsApplication1
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		Private Sub InitializeComponent()
-			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
-			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' chartControl1
-			' 
-			Me.chartControl1.AutoLayout = False
-			Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.chartControl1.Legend.Name = "Default Legend"
-			Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False
-			Me.chartControl1.Location = New System.Drawing.Point(0, 0)
-			Me.chartControl1.Name = "chartControl1"
-			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(){}
-			Me.chartControl1.Size = New System.Drawing.Size(844, 390)
-			Me.chartControl1.TabIndex = 3
-			' 
-			' Form1
-			' 
-			Me.ClientSize = New System.Drawing.Size(844, 390)
-			Me.Controls.Add(Me.chartControl1)
-			Me.Name = "Form1"
-			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		End Sub
-		Private chartControl1 As ChartControl
-	End Class
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+        Private Sub InitializeComponent()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            Me.chartControl1.AutoLayout = False
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Name = "Default Legend"
+            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.[False]
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series(-1) {}
+            Me.chartControl1.Size = New System.Drawing.Size(844, 390)
+            Me.chartControl1.TabIndex = 3
+            ' 
+            ' Form1
+            ' 
+            Me.ClientSize = New System.Drawing.Size(844, 390)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            CType((Me.chartControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
+
+        Private chartControl1 As DevExpress.XtraCharts.ChartControl
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,111 +1,123 @@
-﻿Imports DevExpress.XtraCharts
+Imports DevExpress.XtraCharts
 Imports System
 Imports System.Collections.Generic
-Imports System.Collections.ObjectModel
 Imports System.Windows.Forms
 
 Namespace WindowsFormsApplication1
-	Partial Public Class Form1
-		Inherits Form
 
-		Private Const HistogramPointsCount As Integer = 1000
-		Private Const DistributionPointsCount As Integer = 100
-		Private Const Mean As Double = 0.5
-		Private Const StdDev As Double = 0.15
-		Private Const Max As Double = 10
-		Private Const MinValue As Double = 0
-		Private Const MaxValue As Double = Max
-		Private Const BinCount As Integer = 20
-		Private random As New Random()
+    Public Partial Class Form1
+        Inherits Form
 
-		Private privateNormalDistribution As List(Of DataPoint)
-		Public Property NormalDistribution() As List(Of DataPoint)
-			Get
-				Return privateNormalDistribution
-			End Get
-			Private Set(ByVal value As List(Of DataPoint))
-				privateNormalDistribution = value
-			End Set
-		End Property
-		Private privateHistogram As List(Of DataPoint)
-		Public Property Histogram() As List(Of DataPoint)
-			Get
-				Return privateHistogram
-			End Get
-			Private Set(ByVal value As List(Of DataPoint))
-				privateHistogram = value
-			End Set
-		End Property
+        Private _NormalDistribution As List(Of WindowsFormsApplication1.DataPoint), _Histogram As List(Of WindowsFormsApplication1.DataPoint)
 
+        Const HistogramPointsCount As Integer = 1000
 
-		Public Sub New()
-			InitializeComponent()
-			CreateDataSource()
+        Const DistributionPointsCount As Integer = 100
 
-			Dim histogram As New Series("Histogram", ViewType.Bar)
-			histogram.ArgumentDataMember = "XValue"
-			chartControl1.Series.Add(histogram)
-			CType(histogram.View, BarSeriesView).AggregateFunction = SeriesAggregateFunction.Histogram
+        Const Mean As Double = 0.5
 
-			Dim line As New Series("Line", ViewType.Spline)
-			line.ArgumentDataMember = "XValue"
-			line.ValueDataMembers(0) = "YValue"
-			chartControl1.Series.Add(line)
-			Dim lineView As LineSeriesView = CType(line.View, LineSeriesView)
-			lineView.AggregateFunction = SeriesAggregateFunction.None
-			Dim diagram As XYDiagram = CType(chartControl1.Diagram, XYDiagram)
-			diagram.AxisX.NumericScaleOptions.ScaleMode = ScaleMode.Interval
-			diagram.AxisX.Visibility = DevExpress.Utils.DefaultBoolean.True
-			diagram.AxisX.WholeRange.MinValue = MinValue
-			diagram.AxisX.WholeRange.MaxValue = MaxValue
-			diagram.AxisX.WholeRange.SideMarginsValue = 0
-			diagram.AxisX.NumericScaleOptions.IntervalOptions.GridLayoutMode = GridLayoutMode.GridShiftedLabelCentered
-			diagram.AxisX.NumericScaleOptions.IntervalOptions.Count = BinCount
-			diagram.AxisX.NumericScaleOptions.IntervalOptions.DivisionMode = IntervalDivisionMode.Count
-			diagram.AxisX.Label.TextPattern = "{}{OB}{A1:F1}, {A2:F1}{CB}"
-			Dim secondaryAxisY As New SecondaryAxisY()
-			diagram.SecondaryAxesY.Add(secondaryAxisY)
-			lineView.AxisY = secondaryAxisY
-			Dim secondaryAxisX As New SecondaryAxisX()
-			diagram.SecondaryAxesX.Add(secondaryAxisX)
-			secondaryAxisX.WholeRange.SideMarginsValue = 0
-			lineView.AxisX = secondaryAxisX
+        Const StdDev As Double = 0.15
 
-			line.DataSource = NormalDistribution
-			histogram.DataSource = Histogram
+        Const Max As Double = 10
 
-		End Sub
+        Const MinValue As Double = 0
 
-		Private Sub CreateDataSource()
-			NormalDistribution = New List(Of DataPoint)()
-			For i As Integer = 0 To DistributionPointsCount - 1
-				Dim x As Double = i * Max / DistributionPointsCount
-				NormalDistribution.Add(New DataPoint(x, GetNormalDistribution(x, Mean * Max, StdDev * Max)))
-			Next i
-			Histogram = New List(Of DataPoint)()
-			For x As Integer = 0 To HistogramPointsCount - 1
-				Histogram.Add(New DataPoint(GenerateHistogramPoint(Mean * Max, StdDev * Max)))
-			Next x
-		End Sub
-		Private Function GetNormalDistribution(ByVal x As Double, ByVal mean As Double, ByVal stdDev As Double) As Double
-			Dim tmp As Double = 1 / (Math.Sqrt(2 * Math.PI) * stdDev)
-			Return Math.Exp(-Math.Pow(x - mean, 2) / (2 * Math.Pow(stdDev, 2))) * tmp
-		End Function
-		Private Function GenerateHistogramPoint(ByVal mean As Double, ByVal stdDev As Double) As Double
+        Const MaxValue As Double = Max
 
-			Return Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Cos(2 * Math.PI * random.NextDouble()) * stdDev + mean
-		End Function
-	End Class
-	Public Class DataPoint
-		Public Property XValue() As Double
-		Public Property YValue() As Double
+        Const BinCount As Integer = 20
 
-		Public Sub New(ByVal x As Double, ByVal y As Double)
-			XValue = x
-			YValue = y
-		End Sub
-		Public Sub New(ByVal value As Double)
-			Me.New(value, value)
-		End Sub
-	End Class
+        Private random As Random = New Random()
+
+        Public Property NormalDistribution As List(Of DataPoint)
+            Get
+                Return _NormalDistribution
+            End Get
+
+            Private Set(ByVal value As List(Of DataPoint))
+                _NormalDistribution = value
+            End Set
+        End Property
+
+        Public Property Histogram As List(Of DataPoint)
+            Get
+                Return _Histogram
+            End Get
+
+            Private Set(ByVal value As List(Of DataPoint))
+                _Histogram = value
+            End Set
+        End Property
+
+        Public Sub New()
+            InitializeComponent()
+            CreateDataSource()
+            Dim histogram As Series = New Series("Histogram", ViewType.Bar)
+            histogram.ArgumentDataMember = "XValue"
+            chartControl1.Series.Add(histogram)
+            CType(histogram.View, BarSeriesView).AggregateFunction = SeriesAggregateFunction.Histogram
+            Dim line As Series = New Series("Line", ViewType.Spline)
+            line.ArgumentDataMember = "XValue"
+            line.ValueDataMembers(0) = "YValue"
+            chartControl1.Series.Add(line)
+            Dim lineView As LineSeriesView = CType(line.View, LineSeriesView)
+            lineView.AggregateFunction = SeriesAggregateFunction.None
+            Dim diagram As XYDiagram = CType(chartControl1.Diagram, XYDiagram)
+            diagram.AxisX.NumericScaleOptions.ScaleMode = ScaleMode.Interval
+            diagram.AxisX.Visibility = DevExpress.Utils.DefaultBoolean.True
+            diagram.AxisX.WholeRange.MinValue = MinValue
+            diagram.AxisX.WholeRange.MaxValue = MaxValue
+            diagram.AxisX.WholeRange.SideMarginsValue = 0
+            diagram.AxisX.NumericScaleOptions.IntervalOptions.GridLayoutMode = GridLayoutMode.GridShiftedLabelCentered
+            diagram.AxisX.NumericScaleOptions.IntervalOptions.Count = BinCount
+            diagram.AxisX.NumericScaleOptions.IntervalOptions.DivisionMode = IntervalDivisionMode.Count
+            diagram.AxisX.Label.TextPattern = "{}{OB}{A1:F1}, {A2:F1}{CB}"
+            Dim secondaryAxisY As SecondaryAxisY = New SecondaryAxisY()
+            diagram.SecondaryAxesY.Add(secondaryAxisY)
+            lineView.AxisY = secondaryAxisY
+            Dim secondaryAxisX As SecondaryAxisX = New SecondaryAxisX()
+            diagram.SecondaryAxesX.Add(secondaryAxisX)
+            secondaryAxisX.WholeRange.SideMarginsValue = 0
+            lineView.AxisX = secondaryAxisX
+            line.DataSource = NormalDistribution
+            histogram.DataSource = Me.Histogram
+        End Sub
+
+        Private Sub CreateDataSource()
+            NormalDistribution = New List(Of DataPoint)()
+            For i As Integer = 0 To DistributionPointsCount - 1
+                Dim x As Double = i * Max / DistributionPointsCount
+                NormalDistribution.Add(New DataPoint(x, GetNormalDistribution(x, Mean * Max, StdDev * Max)))
+            Next
+
+            Histogram = New List(Of DataPoint)()
+            For x As Integer = 0 To HistogramPointsCount - 1
+                Histogram.Add(New DataPoint(GenerateHistogramPoint(Mean * Max, StdDev * Max)))
+            Next
+        End Sub
+
+        Private Function GetNormalDistribution(ByVal x As Double, ByVal mean As Double, ByVal stdDev As Double) As Double
+            Dim tmp As Double = 1 / (Math.Sqrt(2 * Math.PI) * stdDev)
+            Return Math.Exp(-Math.Pow(x - mean, 2) / (2 * Math.Pow(stdDev, 2))) * tmp
+        End Function
+
+        Private Function GenerateHistogramPoint(ByVal mean As Double, ByVal stdDev As Double) As Double
+            Return Math.Sqrt(-2 * Math.Log(random.NextDouble())) * Math.Cos(2 * Math.PI * random.NextDouble()) * stdDev + mean
+        End Function
+    End Class
+
+    Public Class DataPoint
+
+        Public Property XValue As Double
+
+        Public Property YValue As Double
+
+        Public Sub New(ByVal x As Double, ByVal y As Double)
+            XValue = x
+            YValue = y
+        End Sub
+
+        Public Sub New(ByVal value As Double)
+            Me.New(value, value)
+        End Sub
+    End Class
 End Namespace
